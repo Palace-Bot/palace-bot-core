513e74e0468f65c714c9187c2440a4b3392bc74d
JiHongYuan <1206429202@qq.com>
2022-05-18 14:51:14 +0800

    feat[core]: test

---
+++ src/test/java/org/github/palace/bot/core/cli/AbstractCommandTest.java
@@ -0,0 +1,43 @@
+package org.github.palace.bot.core.cli;
+
+import org.github.palace.bot.core.annotation.CommandHandler;
+import org.github.palace.bot.core.annotation.CommandPusher;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.test.HelperCommand;
+
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author jihongyuan
+ * @date 2022/5/18 10:32
+ */
+public class AbstractCommandTest {
+    public AbstractCommand command;
+
+    @Before
+    public void constructor() {
+        command = new HelperCommand();
+    }
+
+    @Test
+    public void parse() {
+        command.parse();
+        Map<Method, CommandHandler> commandHandlerMethodMap = command.getCommandHandlerMethodMap();
+
+        Assert.assertNotNull(commandHandlerMethodMap);
+        Assert.assertEquals(1, commandHandlerMethodMap.size());
+
+        Map<Method, CommandPusher> commandPusherMethodMap = command.getCommandPusherMethodMap();
+        Assert.assertNotNull(commandPusherMethodMap);
+        Assert.assertEquals(1, commandPusherMethodMap.size());
+
+        List<AbstractCommand> childrenCommand = command.getChildrenCommand();
+        Assert.assertNotNull(childrenCommand);
+        Assert.assertEquals(1, childrenCommand.size());
+    }
+
+}
---
+++ src/test/java/org/test/HelperCommand.java
@@ -0,0 +1,33 @@
+package org.test;
+
+import net.mamoe.mirai.Bot;
+import net.mamoe.mirai.contact.MemberPermission;
+import org.github.palace.bot.core.annotation.ChildCommandHandler;
+import org.github.palace.bot.core.annotation.CommandHandler;
+import org.github.palace.bot.core.annotation.CommandPusher;
+import org.github.palace.bot.core.cli.AbstractCommand;
+import org.github.palace.bot.core.cli.CommandSender;
+
+/**
+ * @author jihongyuan
+ * @date 2022/5/10 10:08
+ */
+public class HelperCommand extends AbstractCommand {
+
+    public HelperCommand() {
+        super("帮助", MemberPermission.MEMBER, false, "--help");
+    }
+
+    @CommandHandler
+    public void handler(CommandSender commandSender) {
+    }
+
+    @CommandPusher
+    public void pusher(Bot bot) {
+    }
+
+    @ChildCommandHandler(primaryName = "-- help")
+    public void childHelpHandler(CommandSender commandSender) {
+    }
+
+}
